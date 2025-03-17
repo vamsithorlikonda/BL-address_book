b2165db9d33324427aba7ed60c5fe83feda8a7ee
vamsi <vamsithorlikonda99@gmail.com>
2025-03-17 12:17:00 +0530

    [Vamsi] Add:python script for creating_contacts

---
+++ contact.py
@@ -0,0 +1,10 @@
+class Contact:
+      def __init__(self,first_name,last_name,address,city,state,zip_code,phone_number,email_id):
+            self.first_name=first_name
+            self.last_name=last_name
+            self.address=address
+            self.city=city
+            self.state=state
+            self.zip_code=zip_code
+            self.phone_number=phone_number
+            self.email_id=email_id
